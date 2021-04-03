aa688458c0fa8c034f7271dda6cc4ee3f9e9e01d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-04-03 15:48:55 +0200

    Using useLayoutEffect()

--- src/person-editor/usePerson.ts
+++ src/person-editor/usePerson.ts
@@ -1,6 +1,7 @@
 import localforage from "localforage";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { Person } from "../types/person";
+import { sleep } from "../utils";
 
 function savePerson(person: Person | null): void {
   console.log("Saving", person);
@@ -8,15 +9,29 @@ function savePerson(person: Person | null): void {
   localforage.setItem("person", person);
 }
 
+// Note: This is wrong as this value is shared between all instances
+let loaded = false;
+
 export function usePerson(
   initialPerson: Person
 ): [Person | null, (person: Person | null) => void] {
   const [person, setPerson] = useState<Person | null>(null);
 
+  useLayoutEffect(() => {
+    loaded = true;
+
+    return () => {
+      loaded = false;
+    };
+  });
+
   useEffect(() => {
     const getPerson = async () => {
+      await sleep(2500);
       const person = await localforage.getItem<Person>("person");
-      setPerson(person ?? initialPerson);
+      if (loaded) {
+        setPerson(person ?? initialPerson);
+      }
     };
     getPerson();
   }, [initialPerson]);
