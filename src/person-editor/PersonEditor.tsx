8918400d37521befaeef4d2c75f045b09b064cb3
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-04-04 17:07:20 +0200

    The PersonEditor

--- src/person-editor/PersonEditor.tsx
+++ src/person-editor/PersonEditor.tsx
@@ -8,7 +8,7 @@ import { initialPerson } from "../utils";
 import { usePerson } from "./usePerson";
 
 export function PersonEditor(): ReactElement {
-  const [person, setPerson, { isDirty }] = usePerson(initialPerson);
+  const [person, setProperty, { isDirty }] = usePerson(initialPerson);
 
   if (!person) {
     return <Loading />;
@@ -27,35 +27,35 @@ export function PersonEditor(): ReactElement {
         label="Firstname:"
         value={person.firstname}
         onChange={(e) => {
-          setPerson({ ...person, firstname: e.target.value });
+          setProperty("firstname", e.target.value);
         }}
       />
       <LabeledInput
         label="Surname:"
         value={person.surname}
         onChange={(e) => {
-          setPerson({ ...person, surname: e.target.value });
+          setProperty("surname", e.target.value);
         }}
       />
       <LabeledInput
         label="Email:"
         value={person.email}
         onChange={(e) => {
-          setPerson({ ...person, email: e.target.value });
+          setProperty("email", e.target.value);
         }}
       />
       <LabeledInput
         label="Address:"
         value={person.address}
         onChange={(e) => {
-          setPerson({ ...person, address: e.target.value });
+          setProperty("address", e.target.value);
         }}
       />
       <LabeledInput
         label="Phone:"
         value={person.phone}
         onChange={(e) => {
-          setPerson({ ...person, phone: e.target.value });
+          setProperty("phone", e.target.value);
         }}
       />
       <button className="btn btn-primary" disabled={!isDirty}>
